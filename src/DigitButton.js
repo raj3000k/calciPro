46feb392dbd3c19b9103cf3eeec69f275c16de57
Raj Motwani <rajmotwani38@gmail.com>
2023-06-15 20:05:16 +0530

    push code

---
+++ src/DigitButton.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./App";
+import "./styles.css";
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
